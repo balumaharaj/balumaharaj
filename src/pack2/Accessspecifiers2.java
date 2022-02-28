58607e590a979343e8da8b1f3ea6400c6eb4630b
balumaharaj <kavalibalakrishna234@gmail.com>
2022-02-28 16:45:54 +0530

    changes have been committed

---
+++ src/pack2/Accessspecifiers2.java
@@ -0,0 +1,14 @@
+package pack2;
+import pack1.*;
+
+public class Accessspecifiers2 extends Proaccessspecifiers{
+
+	
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			Accessspecifiers2 obj = new Accessspecifiers2 ();   
+		       obj.display();  
+
+	}
+
+}
---
+++ src/pack2/Accessspecifiers3.java
@@ -0,0 +1,15 @@
+package pack2;
+import pack1.*;
+
+public class Accessspecifiers3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+
+
+	}
+
+}
