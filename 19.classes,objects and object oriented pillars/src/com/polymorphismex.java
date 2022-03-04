4a24264903b6ccafee37f14df75c569e26f633ad
balumaharaj <kavalibalakrishna234@gmail.com>
2022-03-04 17:33:22 +0530

    done

---
+++ 19.classes,objects and object oriented pillars/src/com/polymorphismex.java
@@ -0,0 +1,27 @@
+package com;
+
+public class polymorphismex {
+    public int sum(int x, int y) 
+    { 
+        return (x + y); 
+    } 
+    public int sum(int x, int y, int z) 
+    { 
+        return (x + y + z); 
+    } 
+    public double sum(double x, double y) 
+    { 
+        return (x + y); 
+    } 
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		polymorphismex s = new polymorphismex(); 
+        System.out.println(s.sum(10, 20)); 
+        System.out.println(s.sum(10, 20, 30)); 
+        System.out.println(s.sum(10.5, 20.5)); 
+
+	}
+
+}
