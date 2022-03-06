44d9540398d8212ed51483bbc18b93365dcf9b95
balumaharaj <kavalibalakrishna234@gmail.com>
2022-03-06 13:55:22 +0530

    done

---
+++ 01.String to primitive datatypes/src/com/Datatypes.java
@@ -0,0 +1,31 @@
+package com;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				//Converting string to int
+				String s1="34";
+				int a=Integer.parseInt(s1);
+				System.out.println(a);
+				//Converting string to longint
+				String s2="43434343";
+				long l=Long.parseLong(s2);
+				System.out.println(l);
+				//string to float
+				String s3="4343.43";
+				float f=Float.parseFloat(s3);
+				System.out.println(f);
+				//string to boolean
+				String s4="true";
+				Boolean b=Boolean.parseBoolean(s4);
+				System.out.println(s4);
+				//string to char
+				String s5="w";
+				char c=s5.charAt(0);
+				System.out.println(c);
+				
+	}
+
+}
